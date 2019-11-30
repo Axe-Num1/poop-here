662efe4c31466d8ac396238aa3c825ab62a06d88
KangMinseok <qbq5000@gmail.com>
2019-11-30 14:25:24 +0900

    add: + Google map flutter package - 구글 맵 플러터 패키지 추가 - 사용자의 위치 권한 설정 (현재위치 사용)

--- poop_here/ios/Runner/AppDelegate.swift
+++ poop_here/ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCxNmm9HuiX5nuFsG-LKrmzx10SEbw-L24")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
